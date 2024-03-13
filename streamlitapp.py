d7b471cd29499d54b5f16cfbe1861b9644e30194
chetekei <chetekeicollins@gmail.com>
2024-03-13 09:41:17 +0300

    add: views

--- streamlitapp.py
+++ streamlitapp.py
@@ -87,6 +87,15 @@ if uploaded_file is not None:
     # Replace 'NA' values in 'Column1' with values from 'Column2'
     df['Account Involved'] = df['Account Involved'].replace('Nan', np.nan).combine_first(df['Usage'])
 
+    # Define a regex pattern to match everything after the string 'completed'
+    newpattern = r'\bOriginal conversation\b.*'
+
+    # Replace the 'Description' column with the modified values
+    df['Account Involved'] = df['Account Involved'].str.replace(newpattern, '', regex=True)
+
+
+    
+
     df['Amount'] = df['Amount'].str.replace(',', '')
 
     df['Amount'] = pd.to_numeric(df['Amount'])
@@ -100,17 +109,19 @@ if uploaded_file is not None:
     df['Month'] = df['Date'].dt.strftime('%B')
 
     newdf = df[['Date', 'Month', 'Day', 'Time', 'Usage', 'Account Involved', 'Amount']]
+    finaldf = newdf[['Date', 'Time', 'Usage', 'Account Involved', 'Amount']]
 
     payments = newdf[newdf['Amount'] < 0]
     receipts = newdf[newdf['Amount'] > 0]
 
+    payments['Amount'] = payments['Amount'] / -1 
+
     months = receipts.groupby('Month')['Amount'].sum().reset_index()
     day = receipts.groupby('Day')['Amount'].sum().reset_index()
 
-    highest_payment = receipts[receipts['Amount'] == receipts['Amount'].max()]
-    highest = highest_payment['Amount'].iloc[0]
-    highest_description = highest_payment['Account Involved'].iloc[0]
-
+    highest_receipt = receipts[receipts['Amount'] == receipts['Amount'].max()]
+    highest = "{:,.0f}".format(highest_receipt['Amount'].iloc[0])
+    highest_description = highest_receipt['Account Involved'].iloc[0]
     final_description = highest_description.split('-')[-1]
 
     
@@ -118,27 +129,58 @@ if uploaded_file is not None:
     highest_amount_for_month = months['Amount'].max()
     highest_month_row = months[months['Amount'] == highest_amount_for_month]
     highest_month = highest_month_row['Month'].iloc[0]
-    highest_month_amount = highest_month_row['Amount'].iloc[0]
-
-
-
+    highest_month_amount = "{:,.0f}".format(highest_month_row['Amount'].iloc[0])
+   
 
     highest_amount_for_day = day['Amount'].max()
     highest_day_row = day[day['Amount'] == highest_amount_for_day]
     highest_day = highest_day_row['Day'].iloc[0]
-    highest_day_amount = highest_day_row['Amount'].iloc[0]
+    highest_day_amount = "{:,.0f}".format(highest_day_row['Amount'].iloc[0])
 
 
     grouped = receipts.groupby('Usage')['Amount'].sum().reset_index()
-            
+    receipt_account = (receipts.groupby('Account Involved')['Amount'].sum().reset_index())
+    frequent_receipt_account = receipt_account.sort_values(by='Amount', ascending=False).reset_index(drop=True).head(10)
+
+
+    months_payments = payments.groupby('Month')['Amount'].sum().reset_index()
+    day_payments = payments.groupby('Day')['Amount'].sum().reset_index()
+
+    highest_payment = payments[payments['Amount'] == payments['Amount'].max()]
+    format_highest = "{:,.0f}".format(highest_payment['Amount'].iloc[0])
+    highest_description_payments = highest_payment['Account Involved'].iloc[0]
+
+   
+
+    highest_amount_for_month_payments = months_payments['Amount'].max()
+    highest_month_row_payments = months_payments[months_payments['Amount'] == highest_amount_for_month_payments]
+    highest_month_payments = highest_month_row_payments['Month'].iloc[0]
+    highest_month_amount_payments = "{:,.0f}".format(highest_month_row_payments['Amount'].iloc[0])
+   
+
+    highest_amount_for_day_payments = day_payments['Amount'].max()
+    highest_day_row_payments = day_payments[day_payments['Amount'] == highest_amount_for_day_payments]
+    highest_day_payments = highest_day_row_payments['Day'].iloc[0]
+    highest_day_amount_payments = "{:,.0f}".format(highest_day_row_payments['Amount'].iloc[0])
+
+
+    
+    payment_account = (payments.groupby('Account Involved')['Amount'].sum().reset_index())
+    frequent_payment_account = payment_account.sort_values(by='Amount', ascending=False).reset_index(drop=True).head(10)
+
+
+    graph1 = receipts.groupby('Usage')['Amount'].sum().reset_index()
+    graph2 = payments.groupby('Usage')['Amount'].sum().reset_index()
+
+
 
     with card_container(key='global'):
-        tab1, tab2, tab3= st.tabs(["📈 Money Out", "Money In", "🗃 Records"])
+        tab1, tab2, tab3= st.tabs(["📈 Money In", "📈Money Out", "🗃 Records"])
         with tab1:
             with card_container(key='dashboard'):
                 cols = st.columns(3)
                 with cols[0]:
-                    ui.metric_card(title="Highest Payment", content=f'Ksh. {highest}', description= f"{final_description}", key="card1")
+                    ui.metric_card(title="Highest Receipt", content=f'Ksh. {highest}', description= f"{final_description}", key="card1")
                 with cols[1]:
                     ui.metric_card(title="Frequent Month", content=f'{highest_month}', description= f"Ksh. {highest_month_amount}", key="card2")
                 with cols[2]:
@@ -146,15 +188,15 @@ if uploaded_file is not None:
 
 
             with card_container(key='graph'):
-                tab4, tab5  = st.tabs(["Month Analysis", "This Week Analysis"])
+                tab4, tab5  = st.tabs(["Receipts Analysis", "Frequent Accounts"])
                 with tab4:
                     # Calculate the sum of amounts for each category              
                     fig = go.Figure()
 
                     fig.add_trace(go.Bar(
                             width= 0.375,
-                            x = grouped['Usage'],
-                            y= grouped['Amount'],   
+                            x = graph1['Usage'],
+                            y= graph1['Amount'],   
                             name = 'Actual',
                             marker_color="#00A550"
                             
@@ -162,25 +204,67 @@ if uploaded_file is not None:
                     
                             
                     
-                    fig.update_layout(title={'text': 'EXPENDITURE ANALYSIS', 'x': 0.5, 'xanchor': 'center'},  width=900,
+                    fig.update_layout(title={'text': 'RECEIPT ANALYSIS', 'x': 0.5, 'xanchor': 'center'},  width=900,
                                             xaxis_title='Category',
                                             yaxis_title='Amount',
-                                            xaxis=dict(tickfont=dict(size=7.2)),                                  
+                                            xaxis=dict(tickfont=dict(size=9.2)),                                  
                                             )
+                    
+                    fig.update_xaxes(tickangle=90)
 
                     st.plotly_chart(fig)
                 
                 with tab5:
-                    st.table(grouped)
+                    st.table(frequent_receipt_account)
 
-
+        
+    
         with tab2:
-            with card_container(key='form'):        
-                st.table(grouped)
+            with card_container(key='form'):   
+                cols = st.columns(3)
+                with cols[0]:
+                    ui.metric_card(title="Highest Payment", content=f'Ksh. {format_highest}', description= f"{highest_description_payments}", key="card4")
+                with cols[1]:
+                    ui.metric_card(title="Frequent Month", content=f'{highest_month_payments}', description= f"Ksh. {highest_month_amount_payments}", key="card5")
+                with cols[2]:
+                    ui.metric_card(title="Frequent Day", content = f'{highest_day_payments}', description= f"Ksh. {highest_day_amount_payments}", key="card6")
+
+
+            with card_container(key='graph'):
+                tab6, tab7  = st.tabs(["Payment Analysis", "Frequent Accounts"])
+                with tab6:
+                    # Calculate the sum of amounts for each category              
+                    fig2 = go.Figure()
+
+                    fig2.add_trace(go.Bar(
+                            width= 0.375,
+                            x = graph2['Usage'],
+                            y= graph2['Amount'],   
+                            name = 'Actual',
+                            marker_color="#4169e1"
+
+                            
+                            ))               
+                    
+                            
+                    
+                    fig2.update_layout(title={'text': 'PAYMENT ANALYSIS', 'x': 0.5, 'xanchor': 'center'},  width=900,
+                                            xaxis_title='Category',
+                                            yaxis_title='Amount',
+                                            xaxis=dict(tickfont=dict(size=9.2)),                                  
+                                            )
+                    
+                    fig2.update_xaxes(tickangle=90)
+
+                    st.plotly_chart(fig2)
+                
+                with tab7:
+                    st.table(frequent_payment_account)   
+                
 
         with tab3:
             
-            st.table(grouped)
+            st.table(finaldf)
         
 
 
